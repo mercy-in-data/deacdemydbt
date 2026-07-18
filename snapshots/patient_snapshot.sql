47806db5023afd0e79e027b1e8d17dfcf3a62818
Mercy Baffour <mercy_data@outlook.com>
2026-07-18 22:51:27 +0000

    Snapshot source duplicates correction

--- snapshots/patient_snapshot.sql
+++ snapshots/patient_snapshot.sql
@@ -10,8 +10,9 @@
     )
 }}
 
-
-select * from {{source('patient','PATIENT_SRC')}}
-
+with raw as (
+    select *, row_number() over(partition by PATIENT_ID order by CREATED_AT desc) as row_num from {{source('patient','PATIENT_SRC')}}
+)
+select PATIENT_ID, PATIENT_NAME, PATIENT_CONTACT_NUMBER , PATIENT_EMAIL_ID, PATIENT_ADDRESS from raw where row_num = 1
 
 {% endsnapshot %}
