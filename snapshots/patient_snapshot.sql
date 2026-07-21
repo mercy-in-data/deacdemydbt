dd3735dfdcfe6f7a584306157df17efc5c35b9d4
Mercy Baffour <mercy_data@outlook.com>
2026-07-21 18:02:52 +0000

    dbt learn: Added generic tests

--- snapshots/patient_snapshot.sql
+++ snapshots/patient_snapshot.sql
@@ -1,18 +1,14 @@
 {% snapshot patient_snapshot %}
 
-
 {{
     config
     (
-        strategy = 'check',
+        strategy = 'timestamp',
+        updated_at = 'CREATED_AT',
         unique_key = 'PATIENT_ID',
-        check_cols = ['PATIENT_NAME','PATIENT_CONTACT_NUMBER','PATIENT_EMAIL_ID','PATIENT_ADDRESS']
     )
 }}
 
-with raw as (
-    select *, row_number() over(partition by PATIENT_ID order by CREATED_AT desc) as row_num from {{source('patient','PATIENT_SRC')}}
-)
-select PATIENT_ID, PATIENT_NAME, PATIENT_CONTACT_NUMBER , PATIENT_EMAIL_ID, PATIENT_ADDRESS from raw where row_num = 1
+select * from {{source('patient','PATIENT_SRC')}}
 
 {% endsnapshot %}
