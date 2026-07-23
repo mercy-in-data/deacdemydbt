7e2a9fa7169dc4bfa1fc49ba44e89205da18e1ea
Mercy Baffour <mercy_data@outlook.com>
2026-07-23 21:53:53 +0000

    dbt learn: Created macro, cents_to_dollars and used it in stg_stripe_payments, also provided documentation on macro

--- models/dbt_learn/staging/stripe/stg_stripe__payments.sql
+++ models/dbt_learn/staging/stripe/stg_stripe__payments.sql
@@ -3,7 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status as payment_status,
-    amount / 100 as payment_amount,
+    {{ cents_to_dollars("amount") }} as payment_amount,
     created as payment_created,
     _batched_at
 from {{ source('stripe', 'payment') }}
